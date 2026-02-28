719cf689655af74872810e372486ccfa86bc3a9f
Pablo Benjamin Heredia Ruiz <pbheredia@udabol.edu.bo>
2026-02-28 17:26:48 -0400

    fix: remove unused prisma from runtime for vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { ValidationModule } from './validation/validation.module';
 
 @Module({
-  imports: [PrismaModule, ValidationModule],
+  imports: [ValidationModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/validation/validation.service.ts
+++ src/validation/validation.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ValidationService {
-  constructor(private readonly prisma: PrismaService) {}
-
   /**
    * Valida que el ID del usuario sea H22.
    * Retorna información del usuario validado.
